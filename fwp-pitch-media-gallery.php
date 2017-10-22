d3a18e09ef7e00e33eb4e00ae6ba656cc28c56bb
Rad Geek <feedback+finarfin@radgeek.com>
2017-10-22 15:55:01 +0000

    Well let's make a clunky way to get this into the RSS2 feed as well, since that is the typical default.

--- fwp-pitch-media-gallery.php
+++ fwp-pitch-media-gallery.php
@@ -3,19 +3,37 @@
 Plugin Name: FWP+: Pitch Media Gallery
 Plugin URI: http://projects.radgeek.com/
 Description: install on feed producer to transfer URLs for WordPress media galleries across FeedWordPress syndication
-Version: 2012.0322
+Version: 2017.1022
 Author: Charles Johnson
 Author URI: http://radgeek.com/
 License: GPL
 */
 
 class FWPPitchMediaGallery {
-	function __construct () {
+	public function __construct () {
 		add_action('atom_entry', array($this, 'atom_entry'));
+		add_action('rss2_item', array($this, 'rss2_item'));
 	} /* FWPPitchMediaGallery::__construct () */
 	
-	function atom_entry () {
+	public function rss2_item () {
+		$this->attach_gallery('rss2');
+	} /* FWPPitchMediaGallery::rss_item () */
+	
+	public function atom_entry () {
+		$this->attach_gallery('atom');
+	} /* FWPPitchMediaGallery::atom_entry () */
+	
+	public function attach_gallery ($format = 'atom') {
 		global $post;
+		
+		$ns_prefix = '';
+		switch ($format) :
+		case 'atom' :
+			break;
+		case 'rss2' :
+		default :
+			$ns_prefix = 'atom:';	
+		endswitch;
 		
 		// First, let's attach the featured image, if any.
 		$id = get_post_thumbnail_id();
@@ -23,7 +41,7 @@ class FWPPitchMediaGallery {
 		if (intval($id)) :
 			$thumbUrl = wp_get_attachment_url(intval($id));
 			if (is_string($thumbUrl) and strlen($thumbUrl) > 0) :
-				print "\t\t".'<link rel="http://github.com/radgeek/FWPPitchMediaGallery/wiki/thumbnail" href="'.esc_attr($thumbUrl).'" />'."\n";
+				print "\t\t".'<'.$ns_prefix.'link rel="http://github.com/radgeek/FWPPitchMediaGallery/wiki/thumbnail" href="'.esc_attr($thumbUrl).'" />'."\n";
 			endif;
 		endif;
 		
@@ -39,7 +57,7 @@ class FWPPitchMediaGallery {
 			foreach ($attachments as $file) :
 				$url = wp_get_attachment_url($file->ID);
 				if (is_string($url) and strlen($url) > 0) :
-					print "\t\t".'<link rel="enclosure" href="'.esc_attr($url).'" />'."\n";
+					print "\t\t".'<'.$ns_prefix.'link rel="enclosure" href="'.esc_attr($url).'" />'."\n";
 				endif;
 
 			endforeach;
